76ad0a63f0861dc718ddfa3a4f2b890e7251755e
tom <6318275@qq.com>
2017-03-02 18:37:30 +0800

    添加

--- data/IdentityContext.cs
+++ data/IdentityContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace data
 {
-    public class Class1
+    public class IdentityContext:IdentityDbContext<>
     {
     }
 }
---
+++ data/Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data.Models
+{
+    public class AppUser:IdentityUser
+    {
+        public string NikeName { get; set; }
+    }
+}
