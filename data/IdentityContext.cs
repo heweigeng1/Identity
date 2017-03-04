25c9d31dd55ca3e17c88eb674a812b902f3e2795
tom <6318275@qq.com>
2017-03-04 17:47:42 +0800

    IdentityContext

--- data/IdentityContext.cs
+++ data/IdentityContext.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNet.Identity.EntityFramework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using data.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 
 namespace data
 {
-    public class IdentityContext:IdentityDbContext<>
+    public class IdentityContext:IdentityDbContext<AppUser>
     {
+        public IdentityContext() : base("IdentityDb") { }
+        static IdentityContext()
+        {
+            Database.SetInitializer(new Init());
+        }
+        public class Init : DropCreateDatabaseIfModelChanges<IdentityContext> {
+            protected override void Seed(IdentityContext context)
+            {
+                base.Seed(context);
+            }
+        }
     }
 }
